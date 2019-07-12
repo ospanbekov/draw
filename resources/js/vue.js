80304850e6f497dd8425e7d3f9d8c4de1d57b8dc
Askar Ospanbekov <askar.ospanbekov@gmail.com>
2019-07-12 11:46:47 +0600

    front using vue

---
+++ resources/js/vue.js
@@ -0,0 +1,14 @@
+/**
+ * RUN
+ */
+import AppStore from './stores/index';
+
+/* init main app */
+new Vue({
+    el: '#App',
+    store: AppStore,
+    data: () => {
+        return {};
+    },
+    methods: {}
+});
